59484b4662d47652bcc3a26df51127bacfc47143
ReYe0 <1299461580@qq.com>
2022-07-13 14:53:48 +0800

    测试连接数据库成功

--- blog_springboot/src/main/java/com/study/entity/Blog.java
+++ blog_springboot/src/main/java/com/study/entity/Blog.java
@@ -1,7 +1,41 @@
 package com.study.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
+@TableName("tb_blog")
 public class Blog {
+    @TableId(value = "id",type = IdType.AUTO)
+    private Long id;
+    @TableField("title")
+    private String title;
+    @TableField("first_picture")
+    private String firstPicture;
+    @TableField("description")
+    private String description;
+    @TableField("content")
+    private String content;
+    @TableField("create_time")
+    private LocalDateTime createTime;
+    @TableField("update_time")
+    private LocalDateTime updateTime;
+    @TableField("views")
+    private Integer views;
+    @TableField("words")
+    private Integer words;
+    @TableField("type_id")
+    private Long typeId;
+    @TableField("user_id")
+    private Long userId;
+    @TableField("status")
+    private Integer status;
+
 }
--- blog_springboot/src/test/java/com/study/BlogSpringbootApplicationTests.java
+++ blog_springboot/src/test/java/com/study/BlogSpringbootApplicationTests.java
@@ -1,13 +1,27 @@
 package com.study;
 
+import com.study.entity.Blog;
+import com.study.mapper.BlogMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class BlogSpringbootApplicationTests {
 
+	@Autowired
+	private BlogMapper blogMapper;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void test1(){
+		List<Blog> blog = blogMapper.selectList(null);
+		blog.forEach(System.out::println);
 	}
 
 }
